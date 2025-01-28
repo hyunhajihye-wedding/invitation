1597f448022354524259f68aa7b0f33be6efea65
hyunhajihye.wedding@gmail.com <harry.jeon93@gmail.com>
2025-01-28 15:54:04 +0900

    accoutn test

--- src/components/AccountComponent.jsx
+++ src/components/AccountComponent.jsx
@@ -11,7 +11,7 @@ const AccountComponent = () => {
           <div className="account-item-title">신랑측 계좌번호</div>
           <div className="account-item-text">
             <div className="account-item-text-inner">
-              <span className="inner-bank">신한</span>
+              <span className="inner-bank">토스</span>
               <span className="inner-number">123-123-123-123</span>
               <div className="inner-name">전현하</div>
             </div>
