a5faca72bd8fa47348b3b2bfff3bdbef33dfdbc0
hyunhajihye.wedding@gmail.com <harry.jeon93@gmail.com>
2025-02-02 14:27:06 +0900

    env variables for kakao share

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,17 +1,17 @@
 import ComponentWrapper from "./ComponentWrapper";
 import { Share } from "lucide-react";
+import useKakaoShare from "./useKakaoShare";
 
 const FooterComponent = () => {
+  const kakao = useKakaoShare();
   const onShareKakaoClick = () => {
-    console.log("asd");
-    console.log(window.Kakao);
-    window.Kakao.Share.sendDefault({
+    kakao.Share.sendDefault({
       objectType: "feed",
       content: {
         title: "딸기 치즈 케익",
         description: "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
         imageUrl:
-          "http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+          "http://k.kakaocdn.net/dㅡn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
         link: {
           // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
           mobileWebUrl: "https://hyunhajihye-wedding.github.io",
---
+++ src/components/useKakaoShare.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const useKakaoShare = () => {
+  const [kakaoSDK, setKakaoSDK] = useState();
+
+  const KAKAO_CLIENT_ID = import.meta.env.VITE_APP_KAKAO_CLIENT_ID;
+  const KAKAO_INTEGRITY_VALUE = import.meta.env.VITE_APP_KAKAO_INTEGRITY_VALUE;
+  const KAKAO_SDK_VERSION = import.meta.env.VITE_APP_KAKAO_SDK_VERSION;
+
+  const KAKAO_SDK_SRC = `https://t1.kakaocdn.net/kakao_js_sdk/${KAKAO_SDK_VERSION}/kakao.min.js`;
+
+  console.log(KAKAO_CLIENT_ID);
+  console.log(KAKAO_INTEGRITY_VALUE);
+  console.log(KAKAO_SDK_VERSION);
+
+  useEffect(() => {
+    const kakaoSDKURL = `${KAKAO_SDK_SRC}`;
+    let script = document.querySelector(`script[src="${kakaoSDKURL}"]`);
+    if (!script) {
+      script = document.createElement("script");
+      script.src = kakaoSDKURL;
+      script.type = "text/javascript";
+      script.async = true;
+      script.integrity = `${KAKAO_INTEGRITY_VALUE}`;
+      script.crossOrigin = "anonymous";
+      document.head.appendChild(script);
+    }
+
+    const loadHandler = () => {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(KAKAO_CLIENT_ID);
+      }
+      setKakaoSDK(window.Kakao);
+    };
+    script.addEventListener("load", loadHandler);
+  }, [kakaoSDK]);
+
+  return kakaoSDK;
+};
+
+export default useKakaoShare;
